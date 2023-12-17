683bfc4aea3768f52bc3f6763c97d15c30044aa0
Camilo Figueroa <cigotete@gmail.com>
2023-12-17 15:42:27 -0500

    Example of how to obtain a completion.

--- app.py
+++ app.py
@@ -2,7 +2,21 @@ from openai import OpenAI
 
 
 api = OpenAI()
-models = api.models.list()
 
-for model in models:
-    print(model.id)
+next = api.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages = [
+      {
+        "role": "system",
+        "content": "You are a helpful assistant."
+      },
+      {
+        "role": "user",
+        "content": "Once upon a time"
+      }
+    ],
+    max_tokens=7,
+    temperature=0,
+)
+
+print(next)
