992a1d2448083996bedef90aa90d481fb7e12a1c
Camilo Figueroa <cigotete@gmail.com>
2023-12-17 23:05:45 -0500

    Using a version that responses in json format. Needs json word in prompt.

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ from openai import OpenAI
 api = OpenAI()
 
 next = api.chat.completions.create(
-    model="gpt-3.5-turbo",
+    model="gpt-3.5-turbo-1106",
     messages = [
       {
         "role": "system",
-        "content": "You are a helpful assistant."
+        "content": "You are a helpful assistant. Return in json format."
       },
       {
         "role": "user",
-        "content": "Once upon a time"
+        "content": "Give me a list of presidents of the United States."
       }
     ],
-    max_tokens=7,
+    max_tokens=50,
     temperature=0,
+    response_format={ "type": "json_object" },
 )
 
-print(next)
+print(next.choices[0].message.content)
