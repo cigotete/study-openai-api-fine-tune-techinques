43fcf64915c7ed975bb5bb2fc378477390d2adec
Camilo Figueroa <cigotete@gmail.com>
2023-12-21 11:27:01 -0500

    Upload File, and start fine tune process.

--- fine_tune_model.py
+++ fine_tune_model.py
@@ -3,9 +3,8 @@ from openai import OpenAI
 client = OpenAI()
 
 job = client.fine_tuning.jobs.create(
-  training_file="file-XYZ",
-  validation_file="file-ABC",
-  model="davinci-002"
+  training_file="file-IGAJid6dRmDsw8YCGv2I17Db",
+  model="gpt-3.5-turbo-1106"
 )
 
 print("Fine-tuning job started:", job.id)
--- upload_training_data.py
+++ upload_training_data.py
@@ -4,18 +4,10 @@ from openai import OpenAI
 client = OpenAI()
 
 # Upload training file
-with open("drug_malady_data_prepared_train.jsonl", "rb") as training_file:
+with open("drug_malady_data.jsonl", "rb") as training_file:
     training_file_response = client.files.create(
         file=training_file,
         purpose="fine-tune"
     )
 
-# Upload test file
-with open("drug_malady_data_prepared_valid.jsonl", "rb") as test_file:
-    test_file_response = client.files.create(
-        file=test_file,
-        purpose="fine-tune"
-    )
-
 print("Training file ID:", training_file_response.id)
-print("Test file ID:", test_file_response.id)
