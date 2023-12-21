cb121a73cf44b70eb241bc6fcf1410d45f59efee
Camilo Figueroa <cigotete@gmail.com>
2023-12-20 22:20:15 -0500

    Using trained model.

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 import os
+import pprint
 from openai import OpenAI
+
 client = OpenAI()
 
-response = client.completions.create(
-  model="ft:davinci-002:personal::8XcRkM3U",
-  #messages=[
-    #{"role": "system", "content": "You are a helpful assistant."},
-    #{"role": "user", "content": "Hello! When is the library open on weekends?"}
-  #]
-  prompt="When do I have to start the heater?",
-)
-print(response.choices[0].text)
+pp = pprint.PrettyPrinter(indent=2)
+
+drugs = [
+  "What is 'A CN Gel(Topical) 20gmA CN Soap 75gm' used for?", # Class 0
+  "What is 'Addnok Tablet 20'S' used for?", # Class 1
+  "What is 'ABICET M Tablet 10's' used for?", # Class 2
+]
+
+for drug_name in drugs:
+  prompt = "Drug: {}\nMalady:".format(drug_name)
+  response = client.completions.create(
+    model="ft:davinci-002:personal::qwerty",
+    #messages=[
+      #{"role": "system", "content": "You are a helpful assistant."},
+      #{"role": "user", "content": "Hello! When is the library open on weekends?"}
+    #]
+    prompt=prompt,
+    temperature=1,
+    max_tokens=1,
+  )
+  result = vars(response.choices[0])
+  pp.pprint(result)
+  print('#'*50)
