0ae0824a3ea0c2535cfce47afecec14fd8e252a1
Camilo Figueroa <cigotete@gmail.com>
2023-12-20 22:15:46 -0500

    Executed: openai tools fine_tunes.prepare_data -f drug_malady_data.jsonl Results are uploaded.

--- upload_training_data.py
+++ upload_training_data.py
@@ -1,8 +1,21 @@
 import os
 from openai import OpenAI
+
 client = OpenAI()
 
-client.files.create(
-  file=open("data_prepared.jsonl", "rb"),
-  purpose="fine-tune"
-)
+# Upload training file
+with open("drug_malady_data_prepared_train.jsonl", "rb") as training_file:
+    training_file_response = client.files.create(
+        file=training_file,
+        purpose="fine-tune"
+    )
+
+# Upload test file
+with open("drug_malady_data_prepared_valid.jsonl", "rb") as test_file:
+    test_file_response = client.files.create(
+        file=test_file,
+        purpose="fine-tune"
+    )
+
+print("Training file ID:", training_file_response.id)
+print("Test file ID:", test_file_response.id)
