6ca370ae2f2dc4b258d7deeea35dbefb6d40358e
Camilo Figueroa <cigotete@gmail.com>
2023-12-21 12:40:33 -0500

    Using fine tuned model gpt-3.5-turbo-1106. Model answers expected format. Basic eval gives 78% expected correct answers. Data has unbalanced distribution.

--- app.py
+++ app.py
@@ -1,29 +1,54 @@
 import os
-import pprint
 from openai import OpenAI
 
 client = OpenAI()
 
-pp = pprint.PrettyPrinter(indent=2)
+messages = [
+    [
+      {"role": "system", "content": "You are a helpful medical prescription chatbot."},
+      {"role": "user", "content": "What is the use of following drug: 'Admenta 5mg Tablet 10'SAdmenta 10mg Tablet 10'S.'?"} # 3
+    ],
+    [
+      {"role": "system", "content": "You are a helpful medical prescription chatbot."},
+      {"role": "user", "content": "What is the use of following drug: 'Modapro 100mg Tablet 10'SModapro 200mg Tablet 10'S.'?"} #1
+    ],
+    [
+      {"role": "system", "content": "You are a helpful medical prescription chatbot."},
+      {"role": "user", "content": "What is the use of following drug: 'Waklert 150mg Tablet 5'SWaklert 150mg Tablet 10'SWaklert 250mg Tablet 5'SWaklert 50mg Tablet 10'SWaklert 100mg Tablet 10.'?"} # 1
+    ],
+    [
+      {"role": "system", "content": "You are a helpful medical prescription chatbot."},
+      {"role": "user", "content": "What is the use of following drug: 'Ebal 20mg Tablet 10'SEbal 10mg Tablet 10'S'?"} #2
+    ],
+    [
+      {"role": "system", "content": "You are a helpful medical prescription chatbot."},
+      {"role": "user", "content": "What is the use of following drug: 'Lasma LC Kid Tablet 10'S.'?"} #2
+    ],
+    [
+      {"role": "system", "content": "You are a helpful medical prescription chatbot."},
+      {"role": "user", "content": "What is the use of following drug: 'N Citi Plus 500\/800mg Tablet 10'S.'?"} #3
+    ],
+    [
+      {"role": "system", "content": "You are a helpful medical prescription chatbot."},
+      {"role": "user", "content": "What is the use of following drug: 'Tomoxetin 18mg Capsule 10'S.'?"} #1
+    ],
+    [
+      {"role": "system", "content": "You are a helpful medical prescription chatbot."},
+      {"role": "user", "content": "What is the use of following drug: 'Furazole M Tablet 10'S.'?"} #4
+    ],
+    [
+      {"role": "system", "content": "You are a helpful medical prescription chatbot."},
+      {"role": "user", "content": "What is the use of following drug: 'Metrogyl Compound Plus Tablet 10'SMetrogyl Compound Tablet 15'S.'?"} #4
+    ],
+  ]
 
-drugs = [
-  "What is 'A CN Gel(Topical) 20gmA CN Soap 75gm' used for?", # Class 0
-  "What is 'Addnok Tablet 20'S' used for?", # Class 1
-  "What is 'ABICET M Tablet 10's' used for?", # Class 2
-]
-
-for drug_name in drugs:
-  prompt = "Drug: {}\nMalady:".format(drug_name)
-  response = client.completions.create(
-    model="ft:davinci-002:personal::qwerty",
-    #messages=[
-      #{"role": "system", "content": "You are a helpful assistant."},
-      #{"role": "user", "content": "Hello! When is the library open on weekends?"}
-    #]
-    prompt=prompt,
-    temperature=1,
-    max_tokens=1,
+for message in messages:
+  response = client.chat.completions.create(
+    model="ft:gpt-3.5-turbo-1106:personal::8Y67cEkf",
+    messages=message,
+    #prompt=prompt,
+    temperature=0,
+    max_tokens=100,
   )
-  result = vars(response.choices[0])
-  pp.pprint(result)
+  print(response.choices[0].message.content)
   print('#'*50)
